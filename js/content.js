72f86155e5d71ea58f503350ab0e975f339cdb3d
OrangeHubX <71647968+minecraftchicken@users.noreply.github.com>
2022-01-19 00:37:56 +0500

    6.0
    - Fixed broken links in about.html + fixed about me
    - Added content.js for quick text replacement throughout all pages
    - Added announcement banner
    - Added Reev Walls iOS to store page
    - Changed index.html store page banner.
    - Minor changes

---
+++ js/content.js
@@ -0,0 +1,5 @@
+// Announcement Text
+document.getElementById("announcement-title").innerHTML = " Reev wallpapers are now available for iOS!";
+
+// Announcement link text
+document.getElementById("announcement-link").innerHTML = "Check it out";
--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 /* Dark mode */
 
-// Capture the current theme from local storage and adjust the page to use the current theme.
+// Capture current theme from local storage and adjust page to use the current theme.
 const htmlEl = document.getElementsByTagName('html')[0];
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
@@ -8,7 +8,7 @@ if (currentTheme) {
     htmlEl.dataset.theme = currentTheme;
 }
 
-// When the user changes the theme, we need to save the new value on local storage
+// When the user changes the theme, save the new value on local storage
 const toggleTheme = (theme) => {
     htmlEl.dataset.theme = theme;
     localStorage.setItem('theme', theme);
