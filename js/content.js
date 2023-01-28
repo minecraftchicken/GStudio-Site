c9cfeb189306d11cd1e329569a3f01895475c2f3
OrangeHubX <71647968+minecraftchicken@users.noreply.github.com>
2023-01-29 01:43:51 +0500

    reev chroma wip and other minor changes

--- js/content.js
+++ js/content.js
@@ -1,10 +1,10 @@
 // Announcement Text
-document.getElementById("announcement-title").innerHTML = " Catalyst is here! — Experience iOS through KLWP on Android";
+document.getElementById("announcement-title").innerHTML = " Reev Chroma is finally here!";
 
 // Announcement link text
 document.getElementById("announcement-link").innerHTML = "Check it out";
 
 // Announcement link
 function announce_link() {
-    window.location.href = "https://play.google.com/store/apps/details?id=catalyst.grabsterstudios";
+    window.location.href = "https://grabster.tv/ReevChroma";
 }
