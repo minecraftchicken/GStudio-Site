64331961b45842a3291ed474eb1bd8073b762ace
OrangeHubX <71647968+minecraftchicken@users.noreply.github.com>
2024-06-13 23:33:10 +0500

    banner announcement

--- js/content.js
+++ js/content.js
@@ -1,10 +1,10 @@
 // Announcement Text
-document.getElementById("announcement-title").innerHTML = " Reev Chroma is finally here!";
+document.getElementById("announcement-title").innerHTML = " Reev for iOS has arrived!";
 
 // Announcement link text
 document.getElementById("announcement-link").innerHTML = "Check it out";
 
 // Announcement link
 function announce_link() {
-    window.location.href = "https://grabster.tv/ReevChroma";
+    window.location.href = "https://shop.grabster.tv/products/reev-for-ios";
 }
